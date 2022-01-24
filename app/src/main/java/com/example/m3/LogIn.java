ca585d3b7c9d2e0ce927b4e518f379885780a9d3
Atharva Ikhar <atharva464@gmail.com>
2022-01-24 17:53:03 +0530

    Added code for LogIn logic

--- app/src/main/java/com/example/m3/LogIn.java
+++ app/src/main/java/com/example/m3/LogIn.java
@@ -1,22 +1,79 @@
 package com.example.m3;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class LogIn extends AppCompatActivity {
+
+    public EditText EmailField,PasswordField;
+    public Button ForgetPasswordBtn,CreateAccountBtn,LogInBtn;
+    private FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        fAuth = FirebaseAuth.getInstance();
+        ForgetPasswordBtn = findViewById(R.id.ForgetPasswordBtn);
+        CreateAccountBtn = findViewById(R.id.CreateAccountBtn);
+        LogInBtn = findViewById(R.id.LogInBtn);
+        EmailField = findViewById(R.id.EmailField);
+        PasswordField = findViewById(R.id.PasswordField);
 
+        ForgetPasswordBtn.setOnClickListener(view -> {
+            Intent i = new Intent(LogIn.this,ForgetPassword.class);
+            startActivity(i);
+        });
+
+        CreateAccountBtn.setOnClickListener(view -> {
+            Intent i = new Intent(LogIn.this,CreateNewAccount.class);
+            startActivity(i);
+        });
+
+        LogInBtn.setOnClickListener(view -> {
+            String email = EmailField.getText().toString().trim();
+            String password = PasswordField.getText().toString().trim();
+
+            if (TextUtils.isEmpty(email)) {
+                EmailField.setError("Email ID is Required.");
+                return;
+            }
+
+
+            if (TextUtils.isEmpty(password)) {
+                PasswordField.setError("Password is Required.");
+                return;
+            }
+
+            if (password.length() < 6) {
+                PasswordField.setError("Password Must be more than 6 Characters");
+                return;
+            }
+            LogInBtn.setText(R.string.signingin);
+            // authenticate the user
+            fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if (task.isSuccessful())
+                {
+                    Intent intent = new Intent(LogIn.this, Home.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else
+                {
+                    String errorMessage = Objects.requireNonNull(task.getException()).getMessage();
+                    Toast.makeText(LogIn.this, "Error: " + errorMessage, Toast.LENGTH_LONG).show();
+                }
+                LogInBtn.setText(R.string.log_in);
+            });
+        });
     }
 }
--- app/src/main/java/com/example/m3/MainActivity.java
+++ app/src/main/java/com/example/m3/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.m3;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,37 +9,31 @@ import android.os.Handler;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
-    private FirebaseAuth fAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
-    private static int SPLASH_TIME_OUT=1500;
+    private static final int SPLASH_TIME_OUT=1500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
         //Check if user is logged in or not
-        fAuth = FirebaseAuth.getInstance();
-        mAuthStateListener = firebaseAuth -> {
+        FirebaseAuth fAuth = FirebaseAuth.getInstance();
+        FirebaseAuth.AuthStateListener mAuthStateListener = firebaseAuth -> {
             FirebaseUser mFirebaseUser = firebaseAuth.getCurrentUser();
             if (mFirebaseUser != null) {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent i = new Intent(MainActivity.this,Home.class);
-                        startActivity(i);
-                        finish();
-                    }
-                },SPLASH_TIME_OUT);
+                new Handler().postDelayed(() -> {
+                    Intent i = new Intent(MainActivity.this, Home.class);
+                    startActivity(i);
+                    finish();
+                }, SPLASH_TIME_OUT);
             } else {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent i = new Intent(MainActivity.this,LogIn.class);
-                        startActivity(i);
-                        finish();
-                    }
-                },SPLASH_TIME_OUT);
+                new Handler().postDelayed(() -> {
+                    Intent i = new Intent(MainActivity.this, LogIn.class);
+                    startActivity(i);
+                    finish();
+                }, SPLASH_TIME_OUT);
             }
         };
     }
