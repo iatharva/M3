f59a51997689180debb70edbec0b33989f5d478e
Atharva Ikhar <atharva464@gmail.com>
2022-02-07 00:35:42 +0530

    Added Code for Settings Fragment

---
+++ app/src/main/java/com/example/m3/Settings.java
@@ -0,0 +1,19 @@
+package com.example.m3;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+public class Settings extends Fragment {
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_settings, container, false);
+        return view;
+    }
+}
