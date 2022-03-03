d1575899f2a4204386ab37bd273be78e786abedc
Atharva Ikhar <atharva464@gmail.com>
2022-03-04 00:46:15 +0530

    Minor bug fixes and UI updation

---
+++ app/src/main/java/com/example/m3/ProfileOptions/JournalLogs.java
@@ -0,0 +1,14 @@
+package com.example.m3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JournalLogs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journal_logs);
+    }
+}
