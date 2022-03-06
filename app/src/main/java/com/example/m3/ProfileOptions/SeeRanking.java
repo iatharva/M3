e5590f611a4e8ea20181dafa14df3b13beb2f507
Atharva Ikhar <atharva464@gmail.com>
2022-03-06 16:26:24 +0530

    Added code for sharing rank

--- app/src/main/java/com/example/m3/ProfileOptions/SeeRanking.java
+++ app/src/main/java/com/example/m3/ProfileOptions/SeeRanking.java
@@ -5,10 +5,25 @@ import static android.content.ContentValues.TAG;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
 import android.util.Log;
+import android.view.View;
+import android.webkit.PermissionRequest;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -22,7 +37,14 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -37,19 +59,60 @@ public class SeeRanking extends AppCompatActivity {
     public String UID;
     private FirebaseAuth fAuth;
     private ListView listRank;
-    private TextView myRank,myCount,myDate;
+    private TextView myRank,myCount,myDate,myRankTitle;
     private LottieAnimationView animation_view;
+    private ImageView shareRank;
+    private LinearLayout userCard;
     final FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_see_ranking);
         listRank = findViewById(R.id.listRank);
+        myRankTitle = findViewById(R.id.myRankTitle);
         myRank = findViewById(R.id.myPosition);
         myCount = findViewById(R.id.myCount);
+        shareRank = findViewById(R.id.shareRank);
         myDate = findViewById(R.id.myDate);
+        userCard =findViewById(R.id.userCard);
         animation_view = findViewById(R.id.animation_view);
         fAuth = FirebaseAuth.getInstance();
+
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
+        shareRank.setOnClickListener(view -> {
+            Dexter.withActivity(SeeRanking.this).withPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    .withListener(new MultiplePermissionsListener() {
+                        @Override
+                        public void onPermissionsChecked(MultiplePermissionsReport report) {
+                            if (report.areAllPermissionsGranted()) {
+
+                                File file = saveBitMap(SeeRanking.this, userCard);
+                                if (file != null) {
+                                    Log.i("TAG", "Drawing saved to the gallery!");
+
+                                    Intent intent = new Intent(Intent.ACTION_SEND);
+                                    intent.setType("image/*");
+                                    intent.putExtra(Intent.EXTRA_TEXT, "Check out my rank in M3");
+                                    intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+                                    SeeRanking.this.startActivity(Intent.createChooser(intent, "Share Image"));
+
+                                } else {
+                                    Log.i("TAG", "Oops! Image could not be saved.");
+                                    Toast.makeText(SeeRanking.this, "Failed sharing!", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(SeeRanking.this, "Permissions are not granted!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onPermissionRationaleShouldBeShown(List<com.karumi.dexter.listener.PermissionRequest> list, PermissionToken permissionToken) {
+                            permissionToken.continuePermissionRequest();
+                        }
+                    }).check();
+        });
     }
 
     @Override
@@ -196,7 +259,67 @@ public class SeeRanking extends AppCompatActivity {
         else
             animation_view.setAnimation("joggingrankanimation.json");
 
+        String[] userNameSplit = usersNameArray[index-1].split(" ");
+        myRankTitle.setText(userNameSplit[0]+"'s Rank");
         myCount.setText(Integer.toString(userDataArray[index-1])+" logs");
-        myDate.setText(date);
+        String[] dateSplit = date.split(" ");
+        myDate.setText(dateSplit[0]);
+    }
+
+    private File saveBitMap(Context context, View drawView) {
+        File pictureFileDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Handcare"); // enter folder name to save image
+        if (!pictureFileDir.exists()) {
+            boolean isDirectoryCreated = pictureFileDir.mkdirs();
+            if (!isDirectoryCreated)
+                Log.i("ATG", "Can't create directory to save the image");
+            return null;
+        }
+        String filename = pictureFileDir.getPath() + File.separator + System.currentTimeMillis() + ".jpg";
+        File pictureFile = new File(filename);
+        Bitmap bitmap = getBitmapFromView(drawView);
+        try {
+            pictureFile.createNewFile();
+            FileOutputStream oStream = new FileOutputStream(pictureFile);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, oStream);
+            oStream.flush();
+            oStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.i("TAG", "There was an issue saving the image.");
+        }
+        scanGallery(context, pictureFile.getAbsolutePath());
+        return pictureFile;
+    }
+
+    //create bitmap from view and returns it
+    private Bitmap getBitmapFromView(View view) {
+        //Define a bitmap with the same size as the view
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        //Bind a canvas to it
+        Canvas canvas = new Canvas(returnedBitmap);
+        //Get the view's background
+        Drawable bgDrawable = view.getBackground();
+        if (bgDrawable != null) {
+            //has background drawable, then draw it on the canvas
+            bgDrawable.draw(canvas);
+        } else {
+            //does not have background drawable, then draw white background on the canvas
+            canvas.drawColor(Color.WHITE);
+        }
+        // draw the view on the canvas
+        view.draw(canvas);
+        //return the bitmap
+        return returnedBitmap;
+    }
+
+    private void scanGallery(Context cntx, String path) {
+        try {
+            MediaScannerConnection.scanFile(cntx, new String[]{path}, null, new MediaScannerConnection.OnScanCompletedListener() {
+                public void onScanCompleted(String path, Uri uri) {
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
